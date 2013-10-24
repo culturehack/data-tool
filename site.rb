52e20dee296a57b3ed9b6f7b95ba7b1f7a92e6b1
Frankie Roberto <frankie@frankieroberto.com>
2013-10-24 21:05:20 +0100

    Adding basic filtering by category

--- site.rb
+++ site.rb
@@ -1,7 +1,32 @@
 require 'sinatra'
 require 'rdiscount'
 
+SOURCES = [
+  {
+    :title => 'Project Gutenberg',
+    :categories => ['Literature'],
+    :size => 'Medium',
+    :path => '37249992-Project-Gutenberg'
+  }, {
+    :title => 'Open Library',
+    :categories => ['Literature'],
+    :size => 'Huge',
+    :path => '37251000-Open-Library' 
+  } 
+]
+
 get '/' do
+
+  @categories = params[:categories].split(',')
+
+
+  if @categories.size > 0
+
+    @sources = SOURCES.select {|source| (source[:categories] & @categories)[0] }
+  else
+    @sources = SOURCES
+  end
+
   erb :home
 end
 
